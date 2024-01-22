84285fbbe76b7bf4b0f31df0be12d5875d7740ba
farruk12 <farruk.omar@icloud.com>
2024-01-22 00:48:17 -0800

    2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function to sort
+ * @array: param
+ * @size: size of array
+ * Return: nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min  = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
+		print_array(array, size);
+	}
+}
